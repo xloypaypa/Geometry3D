c62bb0003daebfaa998cc0d1b2a1f200ced6acd4
jyk <513303276@qq.com>
2015-02-13 01:03:01 +0800

    add something in the GMatrix.java

--- src/baseType/GMatrix.java
+++ src/baseType/GMatrix.java
@@ -1,4 +1,7 @@
 package baseType;
+
+import mathException.GMatrixSubscriptException;
+
 /**
  * this is a base class of N Dimension Point && Vector;
  * @author jyk
@@ -16,12 +19,19 @@ public class GMatrix {
 	/**
 	 * Create a Matrix by the row and Column;
 	 * @param row
+	 * range from 0 to rows-1;
 	 * the number of rows of the matrix;
 	 * @param column
+	 * range from 0 to columns-1;
 	 * the number of columns of the matrix;
+	 * @throws GMatrixSubscriptException 
 	 */
-	public GMatrix( int row, int column)
+	public GMatrix( int row, int column) throws GMatrixSubscriptException
 	{
+		if( row < 0 || column < 0 )
+		{
+			throw new GMatrixSubscriptException();
+		}
 		this.n = row;
 		this.m = column;
 		this.mat = new float[row*column];
@@ -77,53 +87,102 @@ public class GMatrix {
 	/**
 	 * get the number in the matrix;
 	 * @param row
+	 * range from 0 to rows-1;
 	 * the number of rows of the matrix;
 	 * @param column
+	 * range from 0 to columns-1;
 	 * the number of columns of the matrix;
 	 * @return
 	 * the number at row rows and the column columns in the matrix;
+	 * @throws GMatrixSubscriptException 
 	 */
-	public float get(int row,int column)
+	public float get(int row,int column) throws GMatrixSubscriptException
 	{
-		return mat[row * m +column];
+		if( row < 0 || row >= n || column < 0 ||column >= m )
+		{
+			throw new GMatrixSubscriptException();
+		}
+		else
+		{
+			return mat[row * m +column];
+		}
 	}
 	
 	
 	/**
 	 * set the number in the matrix;
 	 * @param row
+	 * range from 0 to rows-1;
 	 * the number of rows of the matrix;
 	 * @param column
+	 * range from 0 to columns-1;
 	 * the number of columns of the matrix;
 	 * @param f
 	 * the number need set in the matrix;
+	 * @throws GMatrixSubscriptException 
 	 */
-	public void set(int row,int column,float f)
+	public void set(int row,int column,float f) throws GMatrixSubscriptException
 	{
-		mat[row * m +column] = f;
+		if( row < 0 || row >= n || column < 0 ||column >= m )
+		{
+			throw new GMatrixSubscriptException();
+		}
+		else 
+		{
+			mat[row * m +column] = f;
+		}
 	}
 	
 	
 	/**
 	 * 
 	 * @param mat
+	 * @return
 	 */
 	public GMatrix multiplication ( GMatrix mat )
 	{
-		
+		return null;
 	}
 	
 	
+	/**
+	 * 
+	 * @param mat
+	 * @return
+	 */
 	public GMatrix addition ( GMatrix mat )
 	{
-		
+		return null;
 	}
 	
 	
+	/**
+	 * 
+	 * @param mat
+	 * @return
+	 */
 	public GMatrix subtraction ( GMatrix mat )
 	{
+		return null;
 	}
 	
 	
+	/**
+	 * 
+	 * @return
+	 */
+	public GMatrix transpose ( )
+	{
+		return null;
+	}
 
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public boolean equals( )
+	{
+		return false;
+	}
 }
