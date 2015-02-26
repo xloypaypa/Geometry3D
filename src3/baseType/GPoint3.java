958baad52fbb15de65f0c955cba28aa6ab731ec5
jykgod <513303276@qq.com>
2015-02-26 14:54:11 +0800

    simply finish the GPoint3
    something should be added in the future

--- src3/baseType/GPoint3.java
+++ src3/baseType/GPoint3.java
@@ -5,50 +5,72 @@ import mathException.GMatrixSubscriptException;
 public class GPoint3 implements GType {
 
 	protected GMatrix point;
+
+
 	public GPoint3() throws GMatrixSubscriptException{
 		point = new GMatrix( 1 , 3 );
 	}
+
 	public GPoint3( GPoint3 p ) throws GMatrixSubscriptException{
 		point = new GMatrix( p.point );
 	}
+
 	public GPoint3( GMatrix p ) throws GMatrixSubscriptException{
 		point = new GMatrix( p );
 	}
+
 	public GPoint3( float x , float y , float z) throws GMatrixSubscriptException{
 		point = new GMatrix( 1 , 3 );
 		this.setX( x );
 		this.setY( y );
 		this.setZ( z );
 	}
+
 	public GMatrix get(){
 		return point;
 	}
-		
+
 	public float getX() throws GMatrixSubscriptException{
 		return point.get( 0 , 0 );
 	}
+
 	public float getY() throws GMatrixSubscriptException{
 		return point.get( 0 , 1 );
 	}
+
 	public float getZ() throws GMatrixSubscriptException{
 		return point.get( 0 , 2 );
 	}
+
 	public void setX( float x ) throws GMatrixSubscriptException{
 		point.set( 0 , 0 , x );
 	}
+
 	public void setY( float y ) throws GMatrixSubscriptException{
 		point.set( 0 , 1 , y );
 	}
+
 	public void setZ( float z ) throws GMatrixSubscriptException{
 		point.set( 0 , 2 , z );
 	}
+
+	public void set( GPoint3 p) throws GMatrixSubscriptException
+	{
+		this.point = new GMatrix(p.point);
+	}
+
+	public void set( GMatrix p) throws GMatrixSubscriptException
+	{
+		this.point = new GMatrix(p);
+	}
+
 	public void set( float x , float y , float z) throws GMatrixSubscriptException
 	{
 		this.setX( x );
 		this.setY( y );
 		this.setZ( z );
 	}
-	
+
 	@Override
 	public boolean equal(GType obj) {
 		// TODO Auto-generated method stub
